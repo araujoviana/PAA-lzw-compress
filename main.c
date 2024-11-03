8cf3d51621003a680921c094ee5a599716101b63
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-11-03 14:53:52 -0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+** Nome: Matheus Gabriel Viana Araujo
+** RA: 10420444
+** Nome: Enzo Carvalho Pagliarini
+** RA: 10425707
+*/
+
+#include <stdio.h>
+
+char entrada[] = "";
+
+int main(int argc, char *argv[]) {
+  printf("Insira o texto: ");
+
+  return 0;
+}
