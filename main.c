9893385ad15f688654d708d5e74361cbae99e8be
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-11-03 16:56:31 -0300

    Create dictionary array

--- main.c
+++ main.c
@@ -8,6 +8,11 @@
 #include <stdio.h>
 
 char entrada[300];
+char dicionario[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                     'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                     'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                     'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                     's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
 int main(int argc, char *argv[]) {
   printf("Insira o texto: ");
