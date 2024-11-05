0d86475161305bd05573cd9ffd44a05c9cba8b1d
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-11-04 22:01:57 -0300

    Passes the dictionary as arguments

--- main.c
+++ main.c
@@ -10,8 +10,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *compactar_string(char *string_entrada);
-int verificar_presenca(char *sequencia);
+char *compactar_string(char *string_entrada, char *dicionario);
+int verificar_presenca(char *sequencia, char *dicionario);
 
 char entrada[300];
 /*******************************************************************************/
@@ -26,13 +26,12 @@ char entrada[300];
 /*                      's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}; */
 /*******************************************************************************/
 
-
 int main(int argc, char *argv[]) {
 
-// Aloca o dicionário inicial para conter os alfabeto latino em maiúsculo e
-// minúsculo
-int initial_size = 56;
-char *dicionario = malloc(initial_size * sizeof(char));
+  // Aloca o dicionário inicial para conter os alfabeto latino em maiúsculo e
+  // minúsculo
+  int initial_size = 56;
+  char *dicionario = malloc(initial_size * sizeof(char));
 
   // Insere todas as letras dentro do dicionário
   for (int i = 0; i < 26; i++) {
@@ -44,18 +43,18 @@ char *dicionario = malloc(initial_size * sizeof(char));
 
   fgets(entrada, sizeof(entrada), stdin);
 
-  compactar_string(entrada);
+  compactar_string(entrada, dicionario);
 
   return 0;
 }
 
 // Compacta a entrada do usuário usando a compactação LZW
-char *compactar_string(char *string_entrada) {
+char *compactar_string(char *string_entrada, char *dicionario) {
 
   // Itera sobre a string
   char *c = string_entrada;
   while (*c) {
-    if (verificar_presenca(c)) {
+    if (verificar_presenca(c, dicionario)) { // TODO aqui!!! faz ele relocar memória caso n tenha aqui o negocio
       printf("Existe: %c\n", *c);
     } else {
       printf("Não existe: %c\n", *c);
@@ -67,7 +66,7 @@ char *compactar_string(char *string_entrada) {
 }
 
 // TODO mudar esse nome de função
-int verificar_presenca(char *sequencia) {
+int verificar_presenca(char *sequencia, char *dicionario) {
   char *d = dicionario;
   while (*d) {
     if (*d == *sequencia) {
