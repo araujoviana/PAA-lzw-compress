2ec5c96375cb150d02cd951c38bf0750f255a903
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-11-04 22:45:42 -0300

    Prefills dicionario struct

--- main.c
+++ main.c
@@ -8,28 +8,60 @@
 // TODO comentar as coisas
 
 #include <stdio.h>
-// REVIEW pode usar isso?
 #include <stdlib.h>
+// REVIEW pode usar isso?
 #include <string.h>
 
+void preencher_dicionario(char **sequencia, int *indice);
 char *compactar_string(char *string_entrada);
 int verificar_presenca(char *sequencia);
 
+// Entrada do usuário
 char entrada[300];
-char dicionario[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-                     'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-                     'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
-                     'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-                     's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+// Dicionário para armazenar as sequências usadas na compactação da entrada do
+// usuário
+struct dicionario {
+  char **sequencia;
+  int *indice;
+};
 
 int main(int argc, char *argv[]) {
-  printf("Insira o texto: ");
+  int numero_elementos = 56;
 
+  struct dicionario dic;
+  dic.sequencia = malloc(numero_elementos * sizeof(char *));
+
+  preencher_dicionario(dic.sequencia, dic.indice);
+
+  // Lê a entrada do usuário
+  printf("Insira o texto a ser compactado: ");
   fgets(entrada, sizeof(entrada), stdin);
 
   compactar_string(entrada);
 
   return 0;
+}
+
+// Preenche o dicionário com todas as letras do alfabeto latino, maiúsculas e
+// minúsculas, e os indices com suas posições corretas
+void preencher_dicionario(char **sequencia, int *indice) {
+  int contador_indice = 0;
+  for (int i = 0; i < 26; i++) {
+    sequencia[i] = malloc(2 * sizeof(char));
+    sequencia[i][0] = 'A' + i;
+    sequencia[i][1] = '\0';
+    indice[contador_indice] = contador_indice;
+    contador_indice++;
+  }
+
+  for (int i = 0; i < 26; i++) {
+    sequencia[26 + i] = malloc(2 * sizeof(char));
+    sequencia[26 + i][0] = 'a' + i;
+    sequencia[26 + i][1] = '\0';
+    indice[contador_indice] = contador_indice;
+    contador_indice++;
+  }
 }
 
 // Compacta a entrada do usuário usando a compactação LZW
