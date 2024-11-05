ef881e0ae91c0cef8b4e6b892bc498b5a624c3b0
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-11-04 22:49:56 -0300

    Change all function arguments for clarity

--- main.c
+++ main.c
@@ -12,13 +12,6 @@
 // REVIEW pode usar isso?
 #include <string.h>
 
-void preencher_dicionario(char **sequencia, int *indice);
-char *compactar_string(char *string_entrada);
-int verificar_presenca(char *sequencia);
-
-// Entrada do usuário
-char entrada[300];
-
 // Dicionário para armazenar as sequências usadas na compactação da entrada do
 // usuário
 struct dicionario {
@@ -26,13 +19,20 @@ struct dicionario {
   int *indice;
 };
 
+void preencher_dicionario(struct dicionario dic);
+char *compactar_string(char *string_entrada, struct dicionario dic);
+int verificar_presenca(char *sequencia, struct dicionario dic);
+
+// Entrada do usuário
+char entrada[300];
+
 int main(int argc, char *argv[]) {
   int numero_elementos = 56;
 
   struct dicionario dic;
   dic.sequencia = malloc(numero_elementos * sizeof(char *));
 
-  preencher_dicionario(dic.sequencia, dic.indice);
+  preencher_dicionario(dic);
 
   // Lê a entrada do usuário
   printf("Insira o texto a ser compactado: ");
@@ -45,27 +45,27 @@ int main(int argc, char *argv[]) {
 
 // Preenche o dicionário com todas as letras do alfabeto latino, maiúsculas e
 // minúsculas, e os indices com suas posições corretas
-void preencher_dicionario(char **sequencia, int *indice) {
+void preencher_dicionario(struct dicionario dic) {
   int contador_indice = 0;
   for (int i = 0; i < 26; i++) {
-    sequencia[i] = malloc(2 * sizeof(char));
-    sequencia[i][0] = 'A' + i;
-    sequencia[i][1] = '\0';
-    indice[contador_indice] = contador_indice;
+    dic.sequencia[i] = malloc(2 * sizeof(char));
+    dic.sequencia[i][0] = 'A' + i;
+    dic.sequencia[i][1] = '\0';
+    dic.indice[contador_indice] = contador_indice;
     contador_indice++;
   }
 
   for (int i = 0; i < 26; i++) {
-    sequencia[26 + i] = malloc(2 * sizeof(char));
-    sequencia[26 + i][0] = 'a' + i;
-    sequencia[26 + i][1] = '\0';
-    indice[contador_indice] = contador_indice;
+    dic.sequencia[26 + i] = malloc(2 * sizeof(char));
+    dic.sequencia[26 + i][0] = 'a' + i;
+    dic.sequencia[26 + i][1] = '\0';
+    dic.indice[contador_indice] = contador_indice;
     contador_indice++;
   }
 }
 
 // Compacta a entrada do usuário usando a compactação LZW
-char *compactar_string(char *string_entrada) {
+char *compactar_string(char *string_entrada, struct dicionario dic) {
 
   char *w;
   char c;
@@ -88,7 +88,7 @@ char *compactar_string(char *string_entrada) {
 }
 
 // TODO mudar esse nome de função
-int verificar_presenca(char *sequencia) {
+int verificar_presenca(char *sequencia, struct dicionario dic) {
   char *d = dicionario;
   while (*d) {
     if (*d == *sequencia) {
