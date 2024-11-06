d817d6e566c814a5a38f348ea932a79623d84557
araujoviana <63423848+araujoviana@users.noreply.github.com>
2024-11-05 22:27:02 -0300

    Print index values of compressed sequences

--- main.c
+++ main.c
@@ -1,23 +1,10 @@
-// TODO Adicionar mais comentários
-
-/*
-** Nome: Matheus Gabriel Viana Araujo
-** RA: 10420444
-** Nome: Enzo Carvalho Pagliarini
-** RA: 10425707
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Tamanho máximo de uma sequência de caracteres
 #define MAX_SEQUENCIA 100
-// Quantidade de letras no alfabeto latino minúsculo e maiúsculo
 #define TAMANHO_ALFABETO 52
 
-// Dicionário para armazenar as sequências usadas na compactação da entrada do
-// usuário
 struct dicionario {
   char **sequencia; // Conteúdo da sequência
   int *codigo;      // Indice da sequência
@@ -29,30 +16,24 @@ void adicionar_sequencia(struct dicionario *dic, char *sequencia);
 char *compactar_string(char *string_entrada, struct dicionario *dic);
 int verificar_presenca(char *sequencia_atual, struct dicionario *dic);
 
-// Entrada do usuário
 char entrada[300];
 
 int main(int argc, char *argv[]) {
   struct dicionario dic;
   dic.sequencia = malloc(TAMANHO_ALFABETO * sizeof(char *));
-  dic.codigo = malloc(TAMANHO_ALFABETO * sizeof(int *));
+  dic.codigo = malloc(TAMANHO_ALFABETO * sizeof(int));
   dic.tamanho = 0;
 
   preencher_dicionario(&dic);
 
-  // Lê a entrada do usuário
   printf("Insira o texto a ser compactado: ");
   fgets(entrada, sizeof(entrada), stdin);
-
-  // Remove a nova linha da entrada, se existir.
-  // strcspn encontra o indice do caractere '\n'
   entrada[strcspn(entrada, "\n")] = 0;
 
   char *entrada_compactada = compactar_string(entrada, &dic);
   printf("Texto compactado: %s\n", entrada_compactada);
 
-  // Libera a memória do dicionário
-  for (int i = 0; i < TAMANHO_ALFABETO; i++) {
+  for (int i = 0; i < dic.tamanho; i++) {
     free(dic.sequencia[i]);
   }
   free(dic.sequencia);
@@ -61,8 +42,6 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-// Preenche o dicionário com todas as letras do alfabeto latino, maiúsculas e
-// minúsculas
 void preencher_dicionario(struct dicionario *dic) {
   for (int i = 0; i < 26; i++) {
     dic->sequencia[i] = malloc(2 * sizeof(char));
@@ -82,14 +61,20 @@ void preencher_dicionario(struct dicionario *dic) {
 }
 
 void adicionar_sequencia(struct dicionario *dic, char *sequencia) {
+  dic->sequencia = realloc(dic->sequencia, (dic->tamanho + 1) * sizeof(char *));
   dic->sequencia[dic->tamanho] = malloc((strlen(sequencia) + 1) * sizeof(char));
   strcpy(dic->sequencia[dic->tamanho], sequencia);
+
+  int *temp = realloc(dic->codigo, (dic->tamanho + 1) * sizeof(int));
+  if (temp == NULL) {
+      fprintf(stderr, "Memory allocation failed\n");
+      exit(1);
+  }
+  dic->codigo = temp;
   dic->codigo[dic->tamanho] = dic->tamanho;
   dic->tamanho++;
 }
 
-// TODO Adicionar partes não presentes no dicionário a ele! só ta concatenando!
-// Compacta a entrada do usuário usando a compactação LZW
 char *compactar_string(char *string_entrada, struct dicionario *dic) {
   static char sequencia_compactada[MAX_SEQUENCIA] = "";
   char sequencia_atual[MAX_SEQUENCIA] = "";
@@ -102,31 +87,17 @@ char *compactar_string(char *string_entrada, struct dicionario *dic) {
     if (verificar_presenca(sequencia_atual, dic)) {
       char_atual++;
     } else {
-      // Output the code for the current sequence
+      adicionar_sequencia(dic, sequencia_atual);
       snprintf(sequencia_compactada + strlen(sequencia_compactada),
                MAX_SEQUENCIA - strlen(sequencia_compactada), "%d ",
                dic->codigo[verificar_presenca(sequencia_atual, dic) - 1]);
-
-      // Add the new sequence to the dictionary
-      adicionar_sequencia(dic, sequencia_atual);
-
-      // Reset the current sequence
       snprintf(sequencia_atual, sizeof(sequencia_atual), "%c", char_proximo);
       char_atual++;
     }
   }
-
-  // Append the last sequence if there is any
-  if (strlen(sequencia_atual) > 0) {
-    snprintf(sequencia_compactada + strlen(sequencia_compactada),
-             MAX_SEQUENCIA - strlen(sequencia_compactada), "%d ",
-             dic->codigo[verificar_presenca(sequencia_atual, dic) - 1]);
-  }
-
   return sequencia_compactada;
 }
 
-// Verifica se a sequência está presente no dicionário
 int verificar_presenca(char *sequencia_atual, struct dicionario *dic) {
   for (int i = 0; i < dic->tamanho; i++) {
     if (strcmp(sequencia_atual, dic->sequencia[i]) == 0) {
